360bd10937ed2ebb9362dbdd6315fcff36199923
Vaibhav Diwan <divaibhav@gmail.com>
2020-07-28 17:41:48 +0530

    initial and final commit

--- src/MethodDemoIsEven.java
+++ src/MethodDemoIsEven.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
  *  Created by IntelliJ IDEA.
  *  User: Vaibhav
@@ -9,4 +11,21 @@ Create a method isEven(int no1), which accept one argument of type int and
 return a boolean value true if no1 is even otherwise false.
 */
 public class MethodDemoIsEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int no1 = sc.nextInt();
+        MethodDemoIsEven object = new MethodDemoIsEven();
+        boolean result = object.isEven(no1);
+        System.out.println("number is Even -- > " + result);
+    }
+
+    public boolean isEven(int no1) {
+        boolean response = false;
+        if (no1 % 2 == 0) {
+            response = true;
+        }
+        return response;
+
+    }
 }
